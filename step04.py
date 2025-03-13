ced5d274512524e2f472ce7d4d09c0ce1536e9e7
GeonHae Lee <yoandyo2000@gmail.com>
2025-03-13 14:08:24 +0900

    Update step04.py

--- step04.py
+++ step04.py
@@ -1,1 +1,55 @@
 
+import numpy as np
+
+
+class Variable:
+    def __init__(self, data):
+        self.data = data
+
+
+class Function:
+    def __call__(self, input):
+        x = input.data
+        y = self.forward(x)
+        output = Variable(y)
+        self.input = input
+        self.output = output
+        return output
+
+    def forward(self, x):
+        raise NotImplementedError()
+    
+class AddOne(Function):
+    def forward(self,x):
+        return x+1
+
+
+class Square(Function):
+    def forward(self, x):
+        return x ** 2
+
+
+class Exp(Function):
+    def forward(self, x):
+        return np.exp(x)
+
+
+def numerical_diff(f, x, eps=1e-4):
+    x0 = Variable(x.data - eps)
+    x1 = Variable(x.data + eps)
+    y0 = f(x0)
+    y1 = f(x1)
+    return (y1.data - y0.data) / (2 * eps)
+
+
+
+def f(x):
+    A = AddOne()
+    B = Exp()
+    C = Square()
+    return C(A(B(x)))
+
+
+x = Variable(np.array(1.0))
+dy = numerical_diff(f, x)
+print(dy)
