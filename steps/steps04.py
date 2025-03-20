1b2978289425cbca5f2bc816c5657cff2b5cc4e0
GeonHae Lee <yoandyo2000@gmail.com>
2025-03-20 14:29:39 +0900

    Update steps04.py

--- steps/steps04.py
+++ steps/steps04.py
@@ -1,46 +1,54 @@
 import numpy as np
 
+
 class Variable:
-    def __init__(self, data): # __init__ : 초기화 함수, data : 인스턴스 변수
+    def __init__(self, data):
         self.data = data
+
 
 class Function:
     def __call__(self, input):
-        x = input.data # 데이터를 꺼냄
-        y = self.forward(x) # 실제 계산
-        output = Variable(y) # Variable 형태로 되돌림
+        x = input.data
+        y = self.forward(x)
+        output = Variable(y)
+        self.input = input
+        self.output = output
         return output
 
-    def forward(self, x): # Function 클래스의 forward 메서드는 예외를 발생시킴
+    def forward(self, x):
         raise NotImplementedError()
     
+class AddOne(Function):
+    def forward(self,x):
+        return x+1
+
+
 class Square(Function):
     def forward(self, x):
         return x ** 2
 
+
 class Exp(Function):
     def forward(self, x):
         return np.exp(x)
-    
-def numerical_diff(f, x, eps=1e-4): # eps(h) : 앱실론
-    x0 = Variable(x.data - eps) # x - h
-    x1 = Variable(x.data + eps) # x + h
+
+
+def numerical_diff(f, x, eps=1e-4):
+    x0 = Variable(x.data - eps)
+    x1 = Variable(x.data + eps)
     y0 = f(x0)
     y1 = f(x1)
     return (y1.data - y0.data) / (2 * eps)
 
-f = Square() # x^2
-x = Variable(np.array(2.0))
-dy = numerical_diff(f, x) # 2x -> 2*2.0
 
-print(dy)
 
 def f(x):
-    A = Square()
+    A = AddOne()
     B = Exp()
     C = Square()
-    return C(B(A(x))) # (e^x^2)^2
+    return C(A(B(x)))
 
-x = Variable(np.array(0.5))
+
+x = Variable(np.array(1.0))
 dy = numerical_diff(f, x)
 print(dy)
